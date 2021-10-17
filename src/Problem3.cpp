52d6e77f3f624d180c7e324a56aeadbfc93adc0a
Danylo Owl <d.ribchinski@gmail.com>
2021-10-17 14:52:56 -0400

    Started problem 3

---
+++ src/Problem3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+/*
+1. What's the simplest possible input? This is the base case
+    partition(1, 1) = 1
+2. Play around with examples/experiment
+    partition(1, 1) = 1
+    partition(2, 1) = 1
+    partition(3, 1) = 1
+    partition(..., 1) = 1
+3. Relate hard cases to easy cases
+
+4. Generalize the pattern
+
+5. Write code by combining a recursive pattern with the base case
+
+*/
+
+
+// Write a function that counts the number of ways you can partition n objects using parts up to m (assuming m >= 0)
+
+
+int main() {
+
+}
