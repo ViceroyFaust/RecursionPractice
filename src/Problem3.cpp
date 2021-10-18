8f82439c5702fed9c9af1768e5c63088cfa20ec8
Danylo Owl <d.ribchinski@gmail.com>
2021-10-18 18:13:52 -0400

    Problem 3 done

--- src/Problem3.cpp
+++ src/Problem3.cpp
@@ -2,15 +2,15 @@
 
 /*
 1. What's the simplest possible input? This is the base case
-    partition(1, 1) = 1
+    partition(0, 0) = 1
+    if n = 0, then output is always 1
+    if m = 0, then 0
 2. Play around with examples/experiment
-    partition(1, 1) = 1
-    partition(2, 1) = 1
-    partition(3, 1) = 1
-    partition(..., 1) = 1
 3. Relate hard cases to easy cases
-
+    count_partitions(n, m - 1) is subset of count_partitions(n, m)
+    count_partitions(n, m) = count_partitions(n - m, m) + count_partitions(n, m - 1)
 4. Generalize the pattern
+
 
 5. Write code by combining a recursive pattern with the base case
 
@@ -18,8 +18,21 @@
 
 
 // Write a function that counts the number of ways you can partition n objects using parts up to m (assuming m >= 0)
+int count_partitions(int n, int m) {
+    if (n == 0)
+        return 1;
+    if (m == 0 || n < 0)
+        return 0;
+    return count_partitions(n - m, m) + count_partitions(n, m - 1);
 
+}
 
 int main() {
-
+    std::cout << "Partitions\nn: ";
+    int n, m;
+    std::cin >> n;
+    std::cout << "m: ";
+    std::cin >> m;
+    std::cout << "Partitions:  " << count_partitions(n, m) << std::endl;
+    return 0;
 }
